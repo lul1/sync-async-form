878b0fa294e02a43a1ba2aa8ebe5da4235b7bc05
Alumno10 J601 <a10j601@campustls.local>
2017-11-07 12:43:34 -0500

    commit 2

--- public/main.js
+++ public/main.js
@@ -1,11 +1,27 @@
 
 $(document).ready(function(){
     $('input[type="submit"]').on("click",function(event){
-        var first_name = $('input[name="first_name"]').val();
-
-        console.log(first_name);
         event.preventDefault();
+
+        var first_name = $('#first_name').val();
+
+        $.get('find.html',{'first_name':first_name});
+        $.post('find.html',{'first_name':first_name});
+
+
     });
+});
+/*  console.log("Hiciste click!", $('#first_name').val());
+  console.log("Hiciste click!", $('#last_name').val());
+  console.log("Hiciste click!", $('input[name="gender"]:checked').val());
+  console.log("Hiciste click!", $('input[name="interests"]:checked').val());*/
+/*var first_name = $('input[name="first_name"]').val();
 
-
-})
+console.log(first_name);
+event.preventDefault();*/
+/*$.ajax('find.html', {
+  method: 'post',
+  data: {
+    'first_name': first_name
+  }
+});*/

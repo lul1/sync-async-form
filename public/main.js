2cf90adaf6702de23645cf57ddbd9fff68d47bef
kevin <pkevin2007@hotmail.com>
2017-11-13 02:32:13 -0500

    commit tarea

--- public/main.js
+++ public/main.js
@@ -4,18 +4,45 @@ $(document).ready(function(){
         event.preventDefault();
 
         var first_name = $('#first_name').val();
+        var last_name = $('#last_name').val();
+        var femenino = $('#femenino').val();
+        var masculino = $('#masculino').val();
+
+
+
+
+
+
+
+
+
+
 
         $.get('find.html',{'first_name':first_name});
         $.post('find.html',{'first_name':first_name});
+        $.get('find.html',{'last_name':last_name});
+        $.post('find.html',{'last_name':last_name});
+        $.get('find.html',{'femenino':femenino});
+        $.post('find.html',{'femenino':femenino});
+        $.get('find.html',{'masculino':,masculino});
+        $.post('find.html',{'masculino':masculino});
+
+
+
 
 
     });
 });
+
+
 /*  console.log("Hiciste click!", $('#first_name').val());
   console.log("Hiciste click!", $('#last_name').val());
   console.log("Hiciste click!", $('input[name="gender"]:checked').val());
   console.log("Hiciste click!", $('input[name="interests"]:checked').val());*/
 /*var first_name = $('input[name="first_name"]').val();
+
+String RdioValues= request.getParameter("gender");
+String checkboxValues = request.getParameter("Intereses");
 
 console.log(first_name);
 event.preventDefault();*/
